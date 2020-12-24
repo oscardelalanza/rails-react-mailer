d2b0c59fe04d6df142baae43717ce21247403e0e
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-12-24 02:31:38 -0800

    create added

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -4,6 +4,14 @@ class MailsController < ApplicationController
   end
 
   def create
+    user = User.create(user_params)
+    UserMailer.with(user: user).user_mail.deliver_later
     redirect_to root_path, success: 'mail sent'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :subject, :message)
   end
 end
