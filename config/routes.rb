d2d7617d6f067fb26a604b66605bccec25743bd5
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-12-24 02:17:49 -0800

    form created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'mails/new'
+  root to: 'mails#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/users', to: 'mails#create'
 end
