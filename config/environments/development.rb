e28b111117e2d6812794b9bb0697a07a2846ad04
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-12-24 02:47:42 -0800

    succeed!

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV['MAIL'],
+    password:             ENV['PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true }
+  config.action_mailer.raise_delivery_errors = true
 end
